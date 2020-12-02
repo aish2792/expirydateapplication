5968d681db24555a72244affa17c839f4078cb7b
Aishwarya Patil <patilaish2792@gmail.com>
2020-12-02 14:00:36 -0500

    Cleared errors in backend

--- client/components/LoginScreen.js
+++ client/components/LoginScreen.js
@@ -1,20 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, View, Image , SafeAreaView} from 'react-native';
+import { StyleSheet, Text, TextInput, View, Image , SafeAreaView, TouchableOpacity} from 'react-native';
 import { Button, Input } from 'react-native-elements';
 import { useSelector, useDispatch } from 'react-redux';
-import { updateIDs } from '../redux/features/usersSlice'
+import { updateIDs, updateCredentials } from '../redux/features/usersSlice'
 import { Dimensions } from "react-native";
 import colors from '../assets/colors'; 
-import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Formik } from 'formik';
+import * as yup from 'yup';
 
 
-function setIDOf (dispatch) {
-    dispatch(updateIDs('1234'))
+const ReviewSignUpSchema = yup.object({
+    email: yup
+      .string()
+      .min(1, "Must be at least 1 character")
+      .label('Email')
+      .email()
+      .required("Email is a required field"),
+    password: yup
+      .string()
+      .min(6, "Must be at least 6 characters")
+      .label('Password')
+      .required("Password is a required field")
+})
+
+
+// function setIDOf (dispatch) {
+//     dispatch(updateIDs('1234'))
     
-}
+// }
 
-function LoginScreen() {
+function LoginScreen({ navigation }) {
     const dispatch = useDispatch();
     
     return (
@@ -26,7 +41,11 @@ function LoginScreen() {
                 <View>
                     <Formik
                         initialValues={{ email: '', password: ''}}  
-                        onSubmit={(values) => {
+                        validationSchema={ReviewSignUpSchema}
+                        onSubmit={(values, actions) => {
+                            actions.resetForm();
+                            dispatch(updateCredentials(values))
+                             navigation.navigate('Profile');
                             console.log(values);
                         }}  
                     >
@@ -37,22 +56,20 @@ function LoginScreen() {
                                     placeholder='Email'
                                     onChangeText={formikprops.handleChange('email')}
                                     value={formikprops.values.email}
+                                    onBlur={formikprops.handleBlur('email')}
                                 ></TextInput>
+                                <Text style={styles.errorText}>{formikprops.touched.email && formikprops.errors.email}</Text>
                                 <TextInput 
                                     style={styles.inputBox}
                                     placeholder='Password'
                                     onChangeText={formikprops.handleChange('password')}
                                     value={formikprops.values.password}
+                                    onBlur={formikprops.handleBlur('email')}
                                 ></TextInput>
+                                <Text style={styles.errorText}>{formikprops.touched.password && formikprops.errors.password}</Text>
                                 <View style={styles.btnStyle}>
                                     <Button
                                     title='Log in'
-                                    buttonStyle={styles.btn}
-                                    // onPress={() => setIDOf(dispatch)}
-                                    onPress={formikprops.handleSubmit}
-                                    />
-                                    <Button
-                                    title='Cancel'
                                     buttonStyle={styles.btn}
                                     // onPress={() => setIDOf(dispatch)}
                                     onPress={formikprops.handleSubmit}
@@ -62,6 +79,17 @@ function LoginScreen() {
                             </View>
                         )}
                     </Formik>   
+                </View> 
+                <View style={styles.footNote}> 
+                   <Text style={styles.textNote}>New user?</Text> 
+                    <TouchableOpacity
+                        onPress={() => {
+                            console.log("clicked!")
+                            navigation.navigate('Register');
+                          }}
+                    > 
+                        <Text> Sign up!</Text>
+                    </TouchableOpacity>
                 </View>    
             </View>
         </SafeAreaView>
@@ -74,12 +102,13 @@ const styles = StyleSheet.create({
     container: {
         display: 'flex',
         flexDirection: 'column',
-        padding: 2,
+        padding: 10,
         backgroundColor: '#ffffff',
         height: Dimensions.get('window').height-110,
         borderRadius: 40,
         margin: 10,
         justifyContent: 'center',
+        // flex: 1
         // alignItems: 'center'
             
     },
@@ -87,26 +116,31 @@ const styles = StyleSheet.create({
         // flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        padding: 20
+        // padding: 20
 
     },
     formikContainer: {
         // flex: 1,
         // justifyContent: 'flex-',
         // alignItems: 'flex-end'
-        padding: 20
+        // padding: 20
     },
 
     btnStyle: {
         flexDirection: 'row',
-        justifyContent: 'space-evenly',
+        justifyContent: 'center',
         alignItems: 'flex-start',
-        padding: 20
+        
+        // padding: 20
         // flex: 1,
         
     },
     btn: {
-        backgroundColor: colors.bloodred
+        backgroundColor: colors.bloodred,
+        width: '100%',
+        // alignSelf: 'stretch',
+        borderRadius: 100
+        
     },
     fontStyle: {
         fontSize: 60,
@@ -124,7 +158,24 @@ const styles = StyleSheet.create({
         alignItems: 'flex-start',
         justifyContent: 'center'
         
-    }
+    },
+    errorText: {
+        color: colors.bloodred,
+        marginBottom: 10,
+        marginTop: 6,
+        textAlign: 'center'
+        
+    },
+    footNote: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        padding: 10
+        
+    },
+    textNote: {
+        color: colors.silver
+    },
     
 })
 
--- client/redux/features/usersSlice.js
+++ client/redux/features/usersSlice.js
@@ -1,11 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 
+
 export const usersSlice = createSlice({
     name: 'users',
     initialState: {
         id: null,
         isLoggedIn: false,
+        email: '',
+        password: '',
         myProfile: {},
         users: [],
         loading: false,
@@ -16,8 +19,17 @@ export const usersSlice = createSlice({
         setId(state, action) {
 			state.id = action.payload
         },
+        setEmail(state, action) {
+			state.email = action.payload
+        },
+        setPassword(state, action) {
+			state.password = action.payload
+        },
         setItems(state, action) {
 			state.items = action.payload
+        },
+        setMyProfile(state, action) {
+			state.myProfile = action.payload
         },
         setLoading(state, action) {
             state.loading = action.payload;
@@ -28,6 +40,9 @@ export const usersSlice = createSlice({
 export const {
     setId,
     setItems,
+    setEmail,
+    setPassword,
+    setMyProfile,
     setLoading
 } = usersSlice.actions
 
@@ -44,5 +59,40 @@ export const updateIDs = (userId) => async (
       dispatch(setId(userId))
       
   };
+
+export const updateCredentials = (cred) => async (
+    dispatch,
+    getState,
+    ) => {
+        // dispatch(setLoading(true));
+        // dispatch(setEmail(emailId))
+        
+        const {email, password} = cred
+        // console.log(email)
+        // console.log(password)
+        dispatch(setEmail(email))
+        dispatch(setPassword(password))
+        
+    };
+
+export const updateMyProfile = (cred) => async (
+    dispatch,
+    getState,
+    ) => {
+
+        const {email, password, firstname, lastname} = cred
+        // console.log(email)
+        // console.log(password)
+        const profile = {
+            'firstname': firstname,
+            'lastname': lastname,
+            'email': email 
+        }
+        dispatch(setMyProfile(profile))
+        
+        
+
+        
+    };
   
   
