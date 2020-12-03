164685c73b605f95abf924c464bdea28a1c5dda4
Aishwarya Patil <patilaish2792@gmail.com>
2020-12-02 19:35:06 -0500

    Adding again

---
+++ server/serializers.py
@@ -0,0 +1,13 @@
+import rest_framework
+from rest_framework import serializers
+from .models import Users, Items
+
+class UsersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Users
+        fields = ('id', 'firstName', 'lastName', 'userId', 'creationDate')
+
+class ItemsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Items
+        fields = ('id', 'name', 'typeItem', 'expirationDate', 'user_id')
