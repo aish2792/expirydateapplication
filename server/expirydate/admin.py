004561b1d393af5ce80244dcc7ed484fbc82fdd8
Aishwarya Patil <patilaish2792@gmail.com>
2020-12-02 19:48:02 -0500

    all code

---
+++ server/expirydate/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Users, Items
+
+
+
+# We need to register this class with the admin to it which model it is associated with
+@admin.register(Users)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ['firstName', 'lastName', 'userId', 'creationDate']
+
+
+@admin.register(Items)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ['name', 'typeItem', 'expirationDate', 'user']
